885ca893d310287d4c308f2222fafb343eecc4cb
SETHU MADHAVAN P <211614649+sethumadhavan505@users.noreply.github.com>
2026-07-27 19:45:43 +0530

    Delete supabase.js

--- supabase.js
+++
@@ -1,8 +0,0 @@
-const supabaseUrl = "https://jzdjrqufutgkbozxzwbi.supabase.co";
-const supabaseKey = "sb_publishable_eiQjl05lXZ1BsE8fXqx8fQ_9uAjexxW";
-
-// Create your client
-const supabaseClient = window.supabase.createClient(
-    supabaseUrl,
-    supabaseKey
-);
