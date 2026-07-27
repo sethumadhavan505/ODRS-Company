1ca3dc00d277265cae1f968ba4fc0a040ebcd7ec
SETHU MADHAVAN P <211614649+sethumadhavan505@users.noreply.github.com>
2026-07-27 17:05:35 +0530

    Update Supabase URL and key

---
+++ supabase.js
@@ -0,0 +1,7 @@
+const supabaseUrl = "https://jzdjrqufutgkbozxzwbi.supabase.co";
+const supabaseKey = "sb_publishable_eiQjl05lXZ1BsE8fXqx8fQ_9uAjexxW";
+
+const supabase = window.supabase.createClient(
+    supabaseUrl,
+    supabaseKey
+);
